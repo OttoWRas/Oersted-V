c8e6c488787d92e8f4bf8885f41b8d2951e016a2
OttoWRas <otto.westy.rasmussen@gmail.com>
2021-11-08 15:36:05 +0100

    Added intial SCRV

---
+++ src/test/scala/SingleCycleRiscVTester.scala
@@ -0,0 +1,25 @@
+import chisel3.iotesters._
+import org.scalatest._
+
+import Constants._
+
+class RiscVSpec extends FlatSpec with Matchers {
+  "RiscV" should "pass" in {
+    chisel3.iotesters.Driver.execute(Array("--generate-vcd-output", "on"), () => new SingleCycleRiscV()) { c =>
+      new PeekPokeTester(c) {
+
+        def printReg() = {
+          for (i <- 0 until 4) {
+            print(peek(dut.io.regDeb(i)).toString + " ")
+          }
+          println()
+        }
+
+        for (i <- 0 until 4) {
+          printReg()
+          step(1)
+        }
+      }
+    }
+  }
+}

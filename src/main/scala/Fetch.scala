dcf98e9681a146c2741641c408b762c7d65c7ec2
OttoWRas <otto.westy.rasmussen@gmail.com>
2021-11-18 23:01:58 +0100

    Simple fetch component implementation

---
+++ src/main/scala/Fetch.scala
@@ -0,0 +1,54 @@
+package core
+
+import chisel3._
+import chisel3.util._
+
+class ProgramCounter extends Module {
+    val io = IO(new Bundle {
+            val pcPlus   = Input(Bool())
+            val jmpAddr  = Input(UInt(32.W))
+            val wrEnable = Input(Bool())
+            val flagOut  = Output(Bool())
+            val flagIn   = Input(Bool())
+            val pcAddr   = Output(UInt(32.W))
+    })
+
+    val pc = RegInit(0.U(32.W))
+    val flag = RegInit(true.B)
+
+    when (io.wrEnable) {
+        pc := io.jmpAddr
+        flag := true.B
+    }
+
+    when (io.pcPlus) {
+        pc := pc + 4.U
+        flag := true.B 
+    }
+
+    when (io.flagIn) {
+        flag := false.B
+    }
+    io.pcAddr := pc
+    io.flagOut := flag
+}
+
+class InstBuff extends Module {
+    val io = IO(new Bundle {
+            val instIn   = Input(UInt(32.W))
+            val instOut  = Output(UInt(32.W))
+            val flagOut  = Output(Bool())
+            val flagIn   = Input(Bool())
+    })
+
+    val flag = RegInit(false.B)
+
+    val instructionBuffer = RegInit(0.U(32.W))
+    when ((io.instIn =/= instructionBuffer) && (io.instIn =/= 0.U) && io.flagIn) {
+        instructionBuffer := io.instIn
+        flag := false.B
+    }
+    
+    io.flagOut := flag
+    io.instOut := instructionBuffer
+}

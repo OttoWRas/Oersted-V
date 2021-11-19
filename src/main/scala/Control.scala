38a3b4cb408292bd9045e96434b5c08adfd84f96
istlf <is@leek.dk>
2021-11-19 19:57:35 +0100

    Added controller , rote Control test and added instructions.hex.txt file with a few instrs in

---
+++ src/main/scala/Control.scala
@@ -0,0 +1,33 @@
+package core
+
+import chisel3._
+import chisel3.util._
+
+class Control extends Module {
+    val io = IO {
+        new Bundle {
+            val in          = Input(UInt(7.W))
+
+            val branch      = Output(Bool())
+            val memRead     = Output(Bool())
+            val memToReg    = Output(Bool())
+            val ALUOp       = Output(Bool())
+            val memWrite    = Output(Bool())
+            val ALUSrc      = Output(Bool())
+            val regWrite    = Output(Bool())
+        }
+    }
+    
+   io.branch    := io.in(6)
+   io.memRead   := io.in(5)
+   io.memToReg  := io.in(4)
+   io.ALUOp     := io.in(3)
+   io.memWrite  := io.in(2)
+   io.ALUSrc    := io.in(1)
+   io.regWrite  := io.in(0)
+
+}
+
+object ControlGen extends App {
+  (new chisel3.stage.ChiselStage).emitVerilog(new Control())
+}
---
+++ src/test/scala/ControlTest.scala
@@ -0,0 +1,40 @@
+package core
+
+import chisel3.iotesters._
+import org.scalatest._
+import OP._
+
+
+class ControlTester(dut: Control) extends PeekPokeTester(dut) {
+    for(i <- 0 to 100){
+    val r = scala.util.Random
+    
+    val opcodes  = Array(OP_R, OP_I, OP_IL, OP_IE, OP_S, OP_B, OP_JAL, OP_JALR, OP_LUI, OP_AUIPC)
+    val opPoke   = opcodes(r.nextInt(10))
+    
+    val opLit = opPoke.litValue()  
+    val regWrite = opLit & 1
+    val ALUSrc   = opLit & (1<<1)
+    val memWrite = opLit & (1<<2)
+    val ALUOp    = opLit & (1<<3)
+    val memToReg = opLit & (1<<4)
+    val memRead  = opLit & (1<<5)
+    val branch   = opLit & (1<<6)
+
+    poke(dut.io.in, opPoke) // addi x1 x0 2 
+    step(1)
+    expect(dut.io.branch, branch>>6)
+    expect(dut.io.memRead, memRead>>5)
+    expect(dut.io.memToReg, memToReg>>4)
+    expect(dut.io.ALUOp, ALUOp>>3)
+    expect(dut.io.memWrite, memWrite>>2)
+    expect(dut.io.ALUSrc, ALUSrc>>1)
+    expect(dut.io.regWrite, regWrite)
+}
+}
+
+class ControllerSpec extends FlatSpec with Matchers {
+  "Control unit test" should "pass" in {
+    chisel3.iotesters.Driver(() => new Control()) { c => new ControlTester(c)} should be (true)
+  }
+}

a459994120cad598360483ad8d13019c902663c3
OttoWRas <otto.westy.rasmussen@gmail.com>
2021-11-19 15:00:35 +0100

    Simple writeback

---
+++ src/main/scala/WriteBack.scala
@@ -0,0 +1,26 @@
+package core
+
+import chisel3._
+import chisel3.util._
+
+class WriteBack extends Module {
+    val io = IO(new Bundle {
+        val wrEnable = Input(Bool())
+        val memSel   = Input(Bool())
+        val memData  = Input(UInt(32.W))
+        val aluData  = Input(UInt(32.W))
+        val wrBack   = Output(UInt(32.W))
+    })
+
+    io.wrBack := WireDefault(0.U)
+
+    when (wrEnable) {
+        when (memSel) {
+            io.wrBack := io.memData
+        } .otherwise {
+            io.wrBack := io.aluData
+        }
+    }
+
+    
+}

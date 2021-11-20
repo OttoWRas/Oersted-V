72569fb9dc05ce4ada1c685886b2a54b8891a0d4
istlf <is@leek.dk>
2021-11-20 14:37:54 +0100

    More and more ordnung.

--- src/main/scala/SingleCycleRiscV.scala
+++ src/main/scala/SingleCycleRiscV.scala
@@ -5,37 +5,78 @@ import chisel3.util._
 
 class SingleCycleRiscV(program: String = "") extends Module {
   val io = IO(new Bundle {
-    //val regDebug = Output(Vec(32, UInt(32.W))) // debug output for the tester
-    val pcDebug  = Output(UInt(32.W))
-    val instrDebug = Output(UInt(32.W))
+    val regDebug    = Output(Vec(32, UInt(32.W))) // debug output for the tester
+    val pcDebug     = Output(UInt(32.W))
+    val instrDebug  = Output(UInt(32.W))
+   // val done        = Output(Bool())
   })
 
     val mem   = Module(new Memory(program))
     val pc    = Module(new ProgramCounter)
     val ins   = Module(new InstBuff)
     val ctrl  = Module(new Control)
+    val reg   = Module(new Registers)
+    val dec   = Module(new Decoder)
+    val alu   = Module(new ALU)
+    val wb    = Module(new WriteBack)
 
-    pc.io.flagIn := ins.io.flagOut
-    pc.io.pcPlus := true.B
-    pc.io.jmpAddr := 0.U
-    pc.io.wrEnable := false.B
+    /* fetch / initialization */
+    pc.io.flagIn    := ins.io.flagOut
+    pc.io.pcPlus    := true.B
+    pc.io.jmpAddr   := 0.U
+    pc.io.wrEnable  := false.B
     
-    ins.io.flagIn := pc.io.flagOut
-    ins.io.instIn := mem.io.rdData
+    ins.io.flagIn   := pc.io.flagOut
+    ins.io.instIn   := mem.io.rdData
 
-    mem.io.wrEnable := ctrl.io.memWrite //io.wrEnableMem
-    mem.io.wrData   := 0.U //reg.io.rdData2
-    mem.io.wrAddr   := 0.U // alu.io.out
-    mem.io.rdAddr := pc.io.pcAddr>>2 // divide by 4
+    mem.io.wrEnable := ctrl.io.memWrite
+    mem.io.wrData   := reg.io.rdData2
+    mem.io.wrAddr   := alu.io.out
+    mem.io.rdAddr   := pc.io.pcAddr>>2 /* divide by four so we read addresses 1,2,3,4 instead of 0, 4, 8 etc */
 
-    ctrl.io.in := ins.io.instOut
+    ctrl.io.in      := ins.io.instOut
 
+    reg.io.wrEnable := ctrl.io.regWrite
+    reg.io.wrData   := wb.io.wrBack
+    reg.io.wrAddr   := dec.decoded.rd
+    reg.io.rdAddr1  := dec.decoded.rs1
+    reg.io.rdAddr2  := dec.decoded.rs2
 
+    /* decode */
+    dec.io.in       := ins.io.instOut
+
+    /* execute */
+    alu.io.opcode   := dec.io.aluOp
+    alu.io.data1    := reg.io.rdData1
+    alu.io.data2    := WireDefault(0.U)
+    when(ctrl.io.ALUSrc){
+      alu.io.data2 := dec.decoded.imm.asUInt // needs immediate handling
+    }.otherwise {
+      alu.io.data2 := reg.io.rdData2 // this should actually be the immediate 
+    }
+    
+    /* write back */
+    wb.io.memData   := mem.io.wrData 
+    wb.io.aluData   := alu.io.out
+    wb.io.memSel    := ctrl.io.memToReg
+    wb.io.wrEnable  := false.B 
+    when(ctrl.io.memToReg){
+      wb.io.wrEnable := true.B
+    }.otherwise {
+      wb.io.wrEnable := false.B 
+    }
 
 
     /* debugging outputs */
     io.pcDebug := pc.io.pcAddr>>2
     io.instrDebug := ins.io.instOut // mem.io.rdData //instr.io.instOut
+
+    for(i <- 0 to 31){
+      reg.io.wrEnable := false.B 
+      
+      reg.io.rdAddr1 := i.asUInt
+      io.regDebug(i) := reg.io.rdData1
+    }
 }
 
 object CPU extends App {
--- src/test/scala/SingleCycleRiscVTester.scala
+++ src/test/scala/SingleCycleRiscVTester.scala
@@ -8,23 +8,39 @@ import chisel3._
 class RiscVSpec extends FlatSpec with ChiselScalatestTester with Matchers {
   "MAIN tester" should "pass" in {
     test(new SingleCycleRiscV("./testData/instructions.hex.txt")) { m=>
-
     for (w <- 0 to 20) {
-        //m.io.rdAddr.poke((4*w).U)
         m.clock.step(1)
        var pc = m.io.pcDebug.peek().litValue()
        var ins =  m.io.instrDebug.peek().litValue()
         print(f"rdAddr: $pc%x => ")
         print(f"rdData: $ins%8x")
         println()
-        //m.io.rdData.expect(Integer.parseInt(a.slice(w,w+1), 16).U)
+        for(i <- 0 until 3){
+          var v = m.io.regDebug(1).peek().litValue // peek(dut.io.regDebug(i))
+          print(f"x$i%-2d ")
+          print(f"$v%08x \n")
+        }
+         
+
       }
+    //  for (i <- 0 until 31) {
+    //   if(i != 0 && i % 8 == 0) { printf("\n") } 
+
+    //   print(f"x$i%-2d ")
+    //   val v = m.io.regDebug(2).peek().litValue // peek(dut.io.regDebug(i))
+    //   print(f"$v%08x ")
+    // }
+
     println(" ")
     }
   }
 }
 
-
+/*
+   0x00200093, // addi x1 x0 2
+    0x00300113, // addi x2 x0 3
+    0x002081b3) // add x3 x1 x2
+*/
 // class RiscVSpec extends FlatSpec with Matchers {
 //   "RiscV test" should "pass" in {
 //     test(new SingleCycleRiscV).withAnnotations(Seq(WriteVcdAnnotation)) { c=>
@@ -44,16 +60,3 @@ class RiscVSpec extends FlatSpec with ChiselScalatestTester with Matchers {
 // }
 
 
-  // def dump() = {
-  //   for (i <- 0 until 31) {
-  //       if(i != 0 && i % 8 == 0){
-  //     printf("\n")
-  //   }
-
-  //     print(f"x$i%-2d ")
-    
-  //     val v = peek(dut.io.regDebug(i))
- 
-  //     print(f"$v%08x ")
-  //   }
-  // }
